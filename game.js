9e971aa9208cf7205943e8ce4dba537c6b1819cf
luisDiazT <lualdz@gmail.com>
2022-08-17 21:41:40 -0400

    Cambie a colores pasteles, dos funciones para los sonidos, una para el click y otra para generar la secuencia

--- game.js
+++ game.js
@@ -1,13 +1,16 @@
+let userClickedPattern = []
 let gamePattern = []
 let buttonColours = ["red", "blue", "green", "yellow"];
-let randomChosenColour= buttonColours[Math.floor(Math.random()*4)];
-console.log(randomChosenColour)
-crearSecuencia(randomChosenColour);
+let randomChosenColour = Math.floor(Math.random()*4)
+//console.log(randomChosenColour)
+//crearSecuencia(randomChosenColour);
 
-nuevaSecuencia = ()=>{
-let chosenColour = randomChosenColour;
- crearSecuencia(chosenColour);
-}
+  function nextSequence(){
+   let chosenColour = buttonColours[Math.floor(Math.random()*4)];
+    crearSecuencia(chosenColour);
+    gamePattern.push(chosenColour);
+  }
+// //  
 
 let sonidos = [ 
     new Audio("https://drive.google.com/uc?id=1OSSqWcDfm4o6uoCY_LpiVXTzd0u3n0h-"), 
@@ -17,39 +20,75 @@ let sonidos = [
 ]
 
 
-$(`#${randomChosenColour}`).click(function(e){
-  alert(randomChosenColour);
-    // gamePattern.push(randomChosenColour);
-    // gamePattern.forEach(x=>crearSecuencia(x));
-    // nuevaSecuencia()
-  });
+$('.btn').click((event)=>{
+  let userChosenColour = event.target.id;
+  $(`#${userChosenColour}`).toggleClass("pressed")
+  setTimeout(() => {
+  $(`#${userChosenColour}`).toggleClass("pressed") 
+  }, 50);
+  
+  
+  crearSecuencia(userChosenColour)
+  // crearSecuencia(buttonColours[randomChosenColour]);
+  userClickedPattern.push(userChosenColour);
+  userClickedPattern === gamePattern ? nextSequence():$('level-title').html('Perdiste! Jugamos otra vez?')
+});
+  //nextSequence()
+    //console.log(userClickedPattern)
+  //   gamePattern.forEach(x=>{const intervalID = setInterval(myCallback, 1000)
+  //     crearSecuencia(x)
+    
+
+// playSound((color)=>{
+//   switch(color){
+       
+//     case "yellow":
+//     $(`#${color}`).fadeOut(100).fadeIn(100);
+//     sonidos[0].play()
+//     break
+    
+//     case "red":
+//     $(`#${color}`).fadeOut(100).fadeIn(100);
+//     sonidos[1].play()
+//     break
+    
+//     case "blue":
+//     $(`#${color}`).fadeOut(100).fadeIn(100);
+//     sonidos[2].play()
+//     break
+//     case "green":
+//     $(`#${color}`).fadeOut(100).fadeIn(100);    
+//     sonidos[3].play()
+//     break
+// }  
+// });
 
 function crearSecuencia(color){
     switch(color){
        
         case "yellow":
-        $(`#${randomChosenColour}`).fadeOut(100).fadeIn(100);
+        $(`#${color}`).fadeOut(100).fadeIn(100);
         sonidos[0].play()
         break
         
         case "red":
-        $(`#${randomChosenColour}`).fadeOut(100).fadeIn(100);
+        $(`#${color}`).fadeOut(100).fadeIn(100);
         sonidos[1].play()
         break
         
         case "blue":
-        $(`#${randomChosenColour}`).fadeOut(100).fadeIn(100);
+        $(`#${color}`).fadeOut(100).fadeIn(100);
         sonidos[2].play()
         break
         case "green":
-        $(`#${randomChosenColour}`).fadeOut(100).fadeIn(100);    
+        $(`#${color}`).fadeOut(100).fadeIn(100);    
         sonidos[3].play()
         break
     }  
 
-}
+  }
 
-
+/*
 //https://drive.google.com/uc?id=196S_PPkATgYeH5KKumVwA0LGfbJSZ0yf
 
 // const intervalID = setInterval(myCallback, 12000, 'Parameter 1', 'Parameter 2');
@@ -65,4 +104,4 @@ function crearSecuencia(color){
 //  // Parameters are purely optional.
 //  console.log(a);
 //  console.log(b);
-// }
+// */
