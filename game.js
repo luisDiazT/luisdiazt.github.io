5f7f56ad0cbf4e668a0a01ebe54cc1daae4ad3ad
luisDiazT <lualdz@gmail.com>
2022-08-18 21:07:14 -0400

    Se modifica la logica, ya se puede jugar pero no termina el juego

--- game.js
+++ game.js
@@ -1,12 +1,10 @@
-let userClickedPattern = []
-let gamePattern = []
+let userClickedPattern = [];
+let gamePattern = [];
 let buttonColours = ["red", "blue", "green", "yellow"];
+let chequeo = [];
 let randomChosenColour = Math.floor(Math.random()*4)
-let i = 0
+let i = 0;
 let started = false;
-//console.log(randomChosenColour)
-//crearSecuencia(randomChosenColour);
-
 $('html').keypress(()=>{
   if (started===false){
     nextSequence();
@@ -14,95 +12,73 @@ $('html').keypress(()=>{
   }
 })
 
-
-
-
   function nextSequence(){
     i++
-    $('#level-title').fadeOut(200).html(`Nivel ${i}`).fadeIn(300)
+   $('#level-title').fadeOut(200).html(`Nivel ${i}`).fadeIn(300);
    let chosenColour = buttonColours[Math.floor(Math.random()*4)];
     crearSecuencia(chosenColour);
     gamePattern.push(chosenColour);
-  }
-// //  
+    
+    }
 
 let sonidos = [ 
     new Audio("https://drive.google.com/uc?id=1OSSqWcDfm4o6uoCY_LpiVXTzd0u3n0h-"), 
     new Audio("https://drive.google.com/uc?id=1D1wia5vXlMd_hDXZUVe13W4zWhysPSb5"), 
     new Audio("https://drive.google.com/uc?id=1qwpwvmds9NlhWM1EPzvxvF05qNx7dNqq"),
-    new Audio("https://drive.google.com/uc?id=196S_PPkATgYeH5KKumVwA0LGfbJSZ0yf")
+    new Audio("https://drive.google.com/uc?id=196S_PPkATgYeH5KKumVwA0LGfbJSZ0yf"),
+    new Audio("https://drive.google.com/uc?id=1XL_uY_eEYrCiRaHdOUCoLm_nIoOR0-Zb")
 ]
 
 
 $('.btn').click((event)=>{
+  if (started == true){
   let userChosenColour = event.target.id;
   $(`#${userChosenColour}`).toggleClass("pressed")
   setTimeout(() => {
   $(`#${userChosenColour}`).toggleClass("pressed") 
   }, 100);
-  
-  
-  crearSecuencia(userChosenColour)
-  // crearSecuencia(buttonColours[randomChosenColour]);
-  userClickedPattern.push(userChosenColour);
-  let gamePatternTamaño = gamePattern.length-1;
-  console.log(gamePatternTamaño)
-  if (gamePatternTamaño==i){  
-    console.log("holi")
-    checkAnswer(i)
-    userPatternPosition=[]
-  }
+   
+   userClickedPattern.push(userChosenColour)
+   crearSecuencia(userChosenColour)
+   checkAnswer(i) 
 
+  
+  }
 });
-  //nextSequence()
-    //console.log(userClickedPattern)
-  //   gamePattern.forEach(x=>{const intervalID = setInterval(myCallback, 1000)
-  //     crearSecuencia(x)
+
 
 const checkAnswer = ((i)=>{
-let gamePatternPosition = gamePattern[i];
-let userPatternPosition = userClickedPattern[i]
-if(gamePatternPosition == userPatternPosition ){
-  setTimeout(() => {
-    console.log(i)
-    i=i+1
-       nextSequence()
-       
-  }, 1000);
-}else{
-$('#level-title').fadeOut(200).html(`Has perdido :'(`).fadeIn(300)
+
+if(gamePattern[i-1] == userClickedPattern[i-1] ){
+  console.log(`OK`)
+  if (gamePattern.length == userClickedPattern.length){  
+    setTimeout(() => {
+      console.log('El tamaño es igual y esta OK')
+      userClickedPattern = []
+     
+     // gamePattern.forEach((x)=> setTimeout(()=>{crearSecuencia(x)},1000)) POSIBLE REPRODUCCION DE TODO
+      nextSequence()  
+       }, 1000);
+ }
+else if(gamePattern[i-1] != userClickedPattern[i-1]){
+  $('#level-title').fadeOut(200).html(`Has fallado,
+  Quieres intentarlo de nuevo? Presiona cualquier tecla`).fadeIn(300);
+  sonidos[4].play();
 }
 
-})
+}
+else {
+  console.log('not yet')
+}
 
 
-// playSound((color)=>{
-//   switch(color){
-       
-//     case "yellow":
-//     $(`#${color}`).fadeOut(100).fadeIn(100);
-//     sonidos[0].play()
-//     break
-    
-//     case "red":
-//     $(`#${color}`).fadeOut(100).fadeIn(100);
-//     sonidos[1].play()
-//     break
-    
-//     case "blue":
-//     $(`#${color}`).fadeOut(100).fadeIn(100);
-//     sonidos[2].play()
-//     break
-//     case "green":
-//     $(`#${color}`).fadeOut(100).fadeIn(100);    
-//     sonidos[3].play()
-//     break
-// }  
-// });
+}
+)
+
 
 function crearSecuencia(color){
+
     switch(color){
-       
         case "yellow":
         $(`#${color}`).fadeOut(10).fadeIn(10);
         sonidos[0].play()
@@ -125,20 +101,3 @@ function crearSecuencia(color){
 
   }
 
-/*
-//https://drive.google.com/uc?id=196S_PPkATgYeH5KKumVwA0LGfbJSZ0yf
-
-// const intervalID = setInterval(myCallback, 12000, 'Parameter 1', 'Parameter 2');
-
-// function myCallback(a, b)
-// {
-    
-//   
-    
-//     
-//     console.log(gamePattern)
-//  // Your code here
-//  // Parameters are purely optional.
-//  console.log(a);
-//  console.log(b);
-// */
